eeb1e97bdb695246a861d002c61b5bed41463df8
Abiner Maleiane Jr <abinermaleianejr@gmail.com>
2024-07-16 20:44:33 +0200

    Update pay.js

--- Paymentpage/pay.js
+++ Paymentpage/pay.js
@@ -2,7 +2,7 @@ function cardspace(){
     var carddigit = document.getElementById('cardnumber').value;
     if(carddigit.length == 4 || carddigit.length == 9 || carddigit.length ==14){
         document.getElementById('cardnumber').value = carddigit+" ";
-        document.getElementByIdZ('cardnumber').max = 1;
+        document.getElementById('cardnumber').max = 1;
      }
      
 
